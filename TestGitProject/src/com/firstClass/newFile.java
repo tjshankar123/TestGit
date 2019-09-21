c44dc99848c3d3663c8ec6c0361624ffa886194b
tjshankar123 <37399541+tjshankar123@users.noreply.github.com>
2019-09-21 07:49:35 -0400

    new file created.

---
+++ TestGitProject/src/com/firstClass/newFile.java
@@ -0,0 +1,14 @@
+package com.firstClass;
+
+import java.util.Scanner;
+
+public class newFile {
+
+	public static void main(String[] args) {
+		System.out.println("Enter your name:");
+		Scanner in = new Scanner(System.in);
+		System.out.println(in.nextLine().toUpperCase());
+		in.close();	
+	}
+
+}
