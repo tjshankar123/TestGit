bcdef11b6515319d43e12b0a7f7d412167ba75cd
tjshankar123 <37399541+tjshankar123@users.noreply.github.com>
2019-09-21 19:57:12 -0400

    test

--- TestGitProject/src/com/firstClass/newFile.java
+++ TestGitProject/src/com/firstClass/newFile.java
@@ -7,7 +7,7 @@ public class newFile {
 	public static void main(String[] args) {
 		System.out.println("Enter your name:");
 		Scanner in = new Scanner(System.in);
-		System.out.println(in.nextLine());
+		System.out.println(in.nextLine().toLowerCase());
 		in.close();	
 	}
 
