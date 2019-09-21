877e896a4a1f92ee81cfd1d8845ff0d37eb7782a
tjshankar123 <37399541+tjshankar123@users.noreply.github.com>
2019-09-21 07:40:52 -0400

    test

--- TestGitProject/src/com/firstClass/HelloWorld.java
+++ TestGitProject/src/com/firstClass/HelloWorld.java
@@ -3,7 +3,7 @@ package com.firstClass;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.printf("%s\n", "testing");
+		System.out.printf("%s\n", "testing git feature");
 		System.out.println("Hello World");
 
 	}
